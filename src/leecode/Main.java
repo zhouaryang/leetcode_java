f95dfc709f282eee26ecc6a483fe8e7de5e0ca6f
zhouaryang <zhouaryang@tencent.com>
2020-04-11 16:50:43 +0800

    No.2 alph

--- src/leecode/Main.java
+++ src/leecode/Main.java
@@ -1,5 +1,7 @@
 package leecode;
 
+import leecode.solution;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,10 +10,35 @@ public class Main {
 		solution solu =new solution();
 		
 		//No.1 两数之和
-		int nums[] = {2,7,11,15};
+		/*int nums[] = {2,7,11,15};
 		int target = 13;
 		solu.twoSum(nums, target);
 		System.out.println(solu.twoSum(nums, target)[1]);
+		*/
+		
+		func2();
+	}
+	
+	public static void func2(){
+		//No.2 两数相加
+		/* class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; next = null;} //构造函数
+		}*/
+		 ListNode l11 = new ListNode(2);
+		 ListNode l12 = new ListNode(4);l11.next =l12;
+		 ListNode l13 = new ListNode(3);l12.next =l13;
+		 
+		 ListNode l21 = new ListNode(5);
+		 ListNode l22 = new ListNode(6);l21.next = l22;
+		 ListNode l23 = new ListNode(4);l22.next = l23;
+		 ListNode l24 = new ListNode(1);l23.next = l24;
+		 solution solu =new solution(); 
+		 ListNode l6 = solu.addTwoNumbers(l11, l21);
+		 while(l6 != null){
+			 System.out.println("value is "+ l6.val);
+		 }
 	}
 
 }
--- src/leecode/solution.java
+++ src/leecode/solution.java
@@ -3,6 +3,8 @@ package leecode;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.print.attribute.standard.PrinterLocation;
+
 public class solution {
 	
 	/* No.1: 
@@ -46,5 +48,60 @@ public class solution {
 	}
 	
 	
+	/* No.2:
+	 * 给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+		如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+		您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+		示例：
+		输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+		输出：7 -> 0 -> 8
+		原因：342 + 465 = 807
+	 */
+	
+	//内部类 表示链接电费
+		
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		//这个题关键点在于进位处理，有一个标志位记录是否进位，从低位到高位依次相加。
+		ListNode l3 = l1,l4 =l2,l5 = null,l6 =l5;
+		int len1 = 0 ,len2 = 0;
+		int lflag = 0,rflag=0,value=0;//进位表示1,value代表两数之和
+		
+		while(l3 != null){ //
+			len1++;
+			l3 = l3.next;
+		}
+		while(l4 != null){
+			len2++;
+			l4 = l4.next;
+		}
+		for(int i =0;i < Math.max(len1, len2);i++){
+			if(l1 == null){ //如果遍历到尾部
+				l1 = new ListNode(0);
+			}
+			if(l2 == null){
+				l2 = new ListNode(0);
+			}
+			value = l1.val + l2.val;
+			
+			if(value >= 10 ){
+				value = value -10;
+				rflag = 1;
+			}else{
+				rflag = 0;
+			}
+			ListNode l = new ListNode(value+lflag);
+//			System.out.println(value+lflag);
+			l5 =l;l5 = l5.next;
+			l1 = l1.next;l2 = l2.next;lflag = rflag;
+		}
+		return l6;
+
+    }
+	
 	
 }
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null;} //构造函数
+	}
